02a53ab54ac1d88becbac98aa6b7762adf76ac4b
User <trrioiw>
2021-12-10 11:57:42 +0600

    Lms29

---
+++ src/com/company/Dog.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public abstract class Dog {
+public abstract void k();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Integer массивден жуп сандарды терип алынызда
+//        алардын квадраттарынын эн чонун табыныз.
+
+        ArrayList<Integer>integerArrayList=new ArrayList<>(Arrays.asList(2,3,4,5,4,2,1,2,3,5,6,7));
+        System.out.println(integerArrayList);
+        System.out.println();
+
+        List<Integer>L1=integerArrayList.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println( "Жуп сандары "+L1);
+        System.out.println();
+
+     List<Integer> list= L1. stream().map(n ->(int) Math.pow(n, 2)).collect(Collectors.toList());
+        System.out.println(list);
+
+       Integer max=list.stream().max(Integer::compare).get();
+        System.out.println(" Эн чону "+max);
+
+
+    }
+
+
+
+
+}
